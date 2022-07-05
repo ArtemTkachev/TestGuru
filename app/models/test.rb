286f375197caa194dea6b42615e22a19a13aa39d
ArtemTkachev <art59926@gmail.com>
2022-07-05 12:33:05 +0300

    feat: add associations, ER diagram

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_and_belongs_to_many :users, join_table: 'users_tests'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
 
   class << self
     def all_titles_by_category_title(category_title)
