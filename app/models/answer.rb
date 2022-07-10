50f718f6ab10777dee157bb3268c83010569aa52
ArtemTkachev <art59926@gmail.com>
2022-07-10 10:20:22 +0300

    fix: fix validate

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :correcting, -> { where(correct: true) }
-
+  scope :correct_only, -> { where(correct: true) }
   validates :body, presence: true
-  validates :check_max_number_of_answers
+  validate :check_max_number_of_answers
 
   private
 
   def check_max_number_of_answers
-    errors.add(:base, 'The limit of answers to the question has been reached') if question.answers.count > 4
+    errors.add(:base, 'The limit of answers to the question has been reached') if question.answers.count >= 4
   end
 end
