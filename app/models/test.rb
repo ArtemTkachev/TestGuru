2485a1a1d9981a0c9f233f061dbd8c6d314ee1d9
ArtemTkachev <art59926@gmail.com>
2022-07-10 21:32:57 +0300

    fix: fix scope and method

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,14 +5,16 @@ class Test < ApplicationRecord
   has_many :users_tests
   has_many :users, through: :users_tests
 
-  scope :all_titles_by_category_title,
-        ->(category_title) { joins(:category).where(category: { title: category_title })
-        .select(:title).order(title: :desc) }
+  scope :by_category_title,
+        ->(category_title) { joins(:category).where(category: { title: category_title }) }
   scope :simple, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :difficult, -> { where(level: 5..Float::INFINITY) }
 
-  validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :title, uniqueness: { scope: :level }
+  validates :title, presence: true, uniqueness: { scope: :level }
+
+  def self.all_titles_by_category_title(category_title)
+    by_category_title(category_title).order(title: :desc).pluck(:title)
+  end
 end
